a097cfa79d70807f2ebd1b2559b165b6a04f4d83
MoazAhmedS <msayedmohamed@student.eelu.edu.eg>
2023-04-04 13:19:40 -0700

    task0x07 c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
-char *_memset(char *s, char b, unsigned int n){
+char *_memset(char *s, char b, unsigned int n)
+{
 	unsigned int i;
-	for (i = 0; i < n; i++){
+
+	for (i = 0; i < n; i++)
+	{
 		s[i] = b;
 	}
 
