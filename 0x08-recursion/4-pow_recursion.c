d19127ad2b37319230580abb3062039688b14799
MoazAhmedS <msayedmohamed@student.eelu.edu.eg>
2023-06-07 16:43:21 -0700

    task0x08

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - value of x power y
+ * @x: the value
+ * @y: the power
+ *
+ * Return: result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
